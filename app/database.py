d053900c1d7be72fb6dcedae1bc7d2f8fbbf598d
Michael Vanunu <veemikeyy@gmail.com>
2026-07-22 21:01:28 -0400

    set up sqlalchemy and create user model

---
+++ app/database.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autoflush=False,
+    autocommit=False,
+    bind=engine
+)
+
+Base = declarative_base()
--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from app.database import Base, engine
+from app.models.user import User
 
 app = FastAPI()
 
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def root():
