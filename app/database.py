f82c545872ef33eec8bb99f601a82027d6cddb97
Michael Vanunu <veemikeyy@gmail.com>
2026-08-14 17:51:27 -0400

    Fix postgresql driver for deployment

--- app/database.py
+++ app/database.py
@@ -7,7 +7,9 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL.replace("postgresql://", "postgresql+psycopg://")
+)
 
 SessionLocal = sessionmaker(
     autoflush=False,
